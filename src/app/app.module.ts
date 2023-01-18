7847b771c5135ad0e2a33b20da507e396608ffd7
Zentai Pál <zentaipali@gmail.com>
2023-01-18 11:51:13 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RombusComponent } from './rombus/rombus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RombusComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
